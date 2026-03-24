31ef80eb8cf9f0d51e041fc26b21a418984f451f
nikitakrylov <krylovne@mts.ru>
2026-03-24 11:17:12 +0300

    add stages integrations

--- backend/src/app/usecases/get_stages_with_reviewer_and_docs/usecase.py
+++ backend/src/app/usecases/get_stages_with_reviewer_and_docs/usecase.py
@@ -1,5 +1,7 @@
+from entities.document import Document
 from entities.stage_with_reviewer_and_docs import StageWithReviewerAndDocs
 from entities.user import User
+from repositories.inmemory_document_repo import InMemoryDocumentRepository
 from repositories.inmemory_stage_reviewers_repo import AsyncInMemoryStageReviewersRepository
 from repositories.inmemory_stages_repo import AsyncInMemoryStagesRepository
 from repositories.inmemory_user_repo import AsyncInMemoryUserRepository
@@ -9,16 +11,23 @@ class GetStagesWithReviewerAndDocsUseCase:
     def __init__(
         self,
         stages_repo: AsyncInMemoryStagesRepository,
+        document_repo: InMemoryDocumentRepository,
         stage_reviewers_repo: AsyncInMemoryStageReviewersRepository,
         user_repo: AsyncInMemoryUserRepository,
     ):
         self._stages_repo = stages_repo
+        self._document_repo = document_repo
         self._stage_reviewers_repo = stage_reviewers_repo
         self._user_repo = user_repo
 
     async def execute(self) -> list[StageWithReviewerAndDocs]:
         stages = await self._stages_repo.list_all()
+        documents = self._document_repo.get_list()
         stage_reviewers = await self._stage_reviewers_repo.get_list()
+
+        docs_by_stage: dict[int, list[Document]] = {}
+        for doc in documents:
+            docs_by_stage.setdefault(doc.stage_id, []).append(doc)
 
         reviewer_ids_by_stage: dict[int, list[int]] = {}
         for link in stage_reviewers:
@@ -26,6 +35,7 @@ class GetStagesWithReviewerAndDocsUseCase:
 
         out: list[StageWithReviewerAndDocs] = []
         for stage in stages:
+            docs = docs_by_stage.get(stage.stage_id, [])
             reviewer_ids = reviewer_ids_by_stage.get(stage.stage_id, [])
             reviewers: list[User] = []
             for rid in reviewer_ids:
@@ -36,7 +46,7 @@ class GetStagesWithReviewerAndDocsUseCase:
             out.append(
                 StageWithReviewerAndDocs(
                     stage=stage,
-                    docs=[],
+                    docs=docs,
                     reviewers=reviewers,
                 )
             )
